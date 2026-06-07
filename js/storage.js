9879d320188804a3b253c99767753f9e8affdc81
utbaomfal <utbaomfal@skola.malmo.se>
2026-06-07 19:24:46 +0200

    Initialize users from localStorage with default values

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,10 @@
 
+let users = JSON.parse(localStorage.getItem("users")) || [
+  { username: "test", password: "1234", birth: "01/01/2000" }
+];
+
+let currentUser = null;
+
+function save() {
+  localStorage.setItem("users", JSON.stringify(users));
+}
