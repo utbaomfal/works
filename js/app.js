dca5b82a125da62353f0f526c7b8c943f6d9c7e7
utbaomfal <utbaomfal@skola.malmo.se>
2026-06-07 19:25:22 +0200

    Implement chat application functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
 
+function startApp() {
+  document.getElementById("loginPage").classList.add("hidden");
+  document.getElementById("signupPage").classList.add("hidden");
+  document.getElementById("app").classList.remove("hidden");
+
+  document.getElementById("meName").innerText = currentUser.username;
+
+  loadUsers();
+}
+
+function loadUsers() {
+  let list = document.getElementById("userList");
+  list.innerHTML = "";
+
+  users.forEach(u => {
+    let div = document.createElement("div");
+    div.innerText = u.username;
+    div.onclick = () => openChat(u.username);
+    list.appendChild(div);
+  });
+}
+
+let activeChat = null;
+
+function openChat(name) {
+  activeChat = name;
+  document.getElementById("chatBox").innerHTML += 
+    `<p>Chat with ${name}</p>`;
+}
+
+function sendMsg() {
+  let msg = document.getElementById("msg").value;
+
+  if (!msg) return;
+
+  let box = document.getElementById("chatBox");
+  box.innerHTML += `<p><b>${currentUser.username}:</b> ${msg}</p>`;
+
+  document.getElementById("msg").value = "";
+}
