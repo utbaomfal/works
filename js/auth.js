b6d15f0230db6c661af16a82cfc7fca35a3ca1da
utbaomfal <utbaomfal@skola.malmo.se>
2026-06-07 19:25:06 +0200

    Add signup and login functions

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,36 @@
 
+function showSignup() {
+  document.getElementById("loginPage").classList.add("hidden");
+  document.getElementById("signupPage").classList.remove("hidden");
+}
+
+function showLogin() {
+  document.getElementById("signupPage").classList.add("hidden");
+  document.getElementById("loginPage").classList.remove("hidden");
+}
+
+function login() {
+  let u = loginUser.value;
+  let p = loginPass.value;
+
+  let found = users.find(x => x.username === u && x.password === p);
+
+  if (!found) return alert("wrong login");
+
+  currentUser = found;
+  startApp();
+}
+
+function signup() {
+  let u = suUser.value;
+  let p = suPass.value;
+  let b = suBirth.value;
+
+  if (!u || !p || !b) return alert("fill all");
+
+  users.push({ username: u, password: p, birth: b });
+  save();
+
+  alert("account created!");
+  showLogin();
+}
